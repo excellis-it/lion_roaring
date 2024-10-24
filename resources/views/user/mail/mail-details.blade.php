a2f17722c5a4e14e87adb8be08301117b16c725d
Subhasis-Koley <subhasiskoley1995@gmail.com>
2024-10-24 15:59:45 +0530

    css and page

--- resources/views/user/mail/mail-details.blade.php
+++ resources/views/user/mail/mail-details.blade.php
@@ -75,6 +75,79 @@
 
         <div class="mail_text">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+        </div>
+        <div class="mail_reply">
+            <a href=""><span class="material-symbols-outlined">reply</span> Reply</a>
+            <a href=""><span class="material-symbols-outlined">forward</span> Forward</a>
+        </div>
+        <div class="reply_sec">
+            <div class="reply_img_box">
+                <span>
+                    <img src="http://127.0.0.1:8000/user_assets/images/logo.png" alt="user" class="reply_img"/>
+                </span>
+            </div>
+            <div class="reply_text_box">
+                <div class="">
+                    <div class="d-flex align-items-center"><span class="material-symbols-outlined">reply</span> &nbsp;&nbsp; | &nbsp;&nbsp; subhasiskoley@gmail.com</div>
+                </div>
+                <div class="big_textara">
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <a href="" class="send_btn">Send</a>
+                        <div class="file-input">
+                            <input type="file" name="file-input" id="file-input" class="file-input__input">
+                            <label class="file-input__label" for="file-input">
+                                <span><i class="fa fa-paperclip"></i></span></label>
+                        </div>
+                    </div>
+                    <div class="trash_btn">
+                        <a href="" class=""><i class='fa fa-trash'></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="reply_sec">
+            <div class="reply_img_box">
+                <span>
+                    <img src="http://127.0.0.1:8000/user_assets/images/logo.png" alt="user" class="reply_img"/>
+                </span>
+            </div>
+            <div class="reply_text_box">
+                <div class="">
+                    <div class="d-flex align-items-center"><span class="material-symbols-outlined">forward</span> &nbsp;&nbsp; | &nbsp;&nbsp; <input type="text" class="text_box"/></div>
+                </div>
+                <div class="big_textara">
+                    <textarea name="" id="" cols="30" rows="5" placeholder="Message"></textarea>
+                </div>
+                <div class="fowroad">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam tempora iure sed, libero sunt, cumque alias porro inventore dolorem, consequuntur culpa nihil iusto rem! Optio reiciendis in dignissimos vel necessitatibus.</p>
+                </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div>
+                        <a href="" class="send_btn">Send</a>
+                        <div class="file-input">
+                            <input type="file" name="file-input" id="file-input" class="file-input__input">
+                            <label class="file-input__label" for="file-input">
+                                <span><i class="fa fa-paperclip"></i></span></label>
+                        </div>
+                    </div>
+                    <div class="trash_btn">
+                        <a href="" class=""><i class='fa fa-trash'></i></a>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Email List rows Ends -->
