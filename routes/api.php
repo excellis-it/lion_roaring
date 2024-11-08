b800aaa7d7c1635d4c6a2f873795921cbb8908ad
excellis-it <web.excellisit@gmail.com>
2024-11-08 19:28:12 +0530

    add api 12

--- routes/api.php
+++ routes/api.php
@@ -30,50 +30,6 @@ use App\Http\Controllers\Api\TopicController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// Route::prefix('v1')->group(function () {
-//     Route::post('contact-us', [ContactUsController::class, 'store']);
-
-//     Route::prefix('cms')->group(function () {
-//         Route::post('home', [CmsController::class, 'home']);
-//         Route::post('gallery', [CmsController::class, 'gallery']);
-//         Route::post('contact-us', [CmsController::class, 'contactUs']);
-//         Route::post('faq', [CmsController::class, 'faq']);
-//         Route::post('principle-and-business', [CmsController::class, 'principleAndBusiness']);
-//         Route::post('ecclesia-associations', [CmsController::class, 'ecclesiaAssociations']);
-//         Route::post('our-organization', [CmsController::class, 'ourOrganization']);
-//         Route::post('common', [CmsController::class, 'common']);
-//         Route::post('our-governance', [CmsController::class, 'ourGovernance']);
-//         Route::post('organization-center', [CmsController::class, 'organizationCenter']);
-//         Route::post('organization-center-details', [CmsController::class, 'organizationCenterDetails']);
-//         Route::post('members-privacy-policy', [CmsController::class, 'membersPrivacyPolicy']);
-//     });
-//     // donation
-//     Route::post('donation', [DonationController::class, 'donation']);
-//     Route::post('country-list', [DonationController::class, 'countryList']);
-//     Route::post('login', [AuthController::class, 'login']);
-//     Route::post('register', [AuthController::class, 'register']);
-//     Route::post('register-agreement', [AuthController::class, 'registerAgreement']);
-//     Route::post('forget-password', [ForgetPasswordController::class, 'forgetPassword']);
-
-//     Route::group(['middleware' => ['auth:api'], 'prefix' => 'user'], function () {
-//         Route::prefix('subscription')->group(function () {
-//         Route::post('details', [SubscriptionController::class, 'details']);
-//         Route::post('subscribe', [SubscriptionController::class, 'subscribe']);
-//         // api.stripe.checkout.success
-//         Route::get('stripe-checkout-success', [SubscriptionController::class, 'stripeCheckoutSuccess'])->name('api.stripe.checkout.success');
-//         });
-//         Route::middleware(['api.member.access'])->group(function () {
-//             Route::post('profile', [ProfileController::class, 'profile']);
-//             Route::post('update-profile', [ProfileController::class, 'updateProfile']);
-//             Route::post('profile-picture-update', [ProfileController::class, 'profilePictureUpdate']);
-//             Route::post('change-password', [ProfileController::class, 'changePassword']);
-//         });
-//     });
-// });
 
 
 
@@ -132,10 +88,10 @@ Route::prefix('v3')->group(function () {
             Route::post('/name-des-update', [TeamChatController::class, 'nameDescriptionUpdate']);
             Route::post('/remove-member', [TeamChatController::class, 'removeMember']);
             Route::post('/add-member-team', [TeamChatController::class, 'addMemberTeam']);
-            Route::post('/exit-from-group', [TeamChatController::class, 'exitFromGroup']);            
+            Route::post('/exit-from-group', [TeamChatController::class, 'exitFromGroup']);
             Route::post('/delete-group', [TeamChatController::class, 'deleteGroup']);
             Route::post('/make-admin', [TeamChatController::class, 'makeAdmin']);
-            Route::post('/seen', [TeamChatController::class, 'seen']);            
+            Route::post('/seen', [TeamChatController::class, 'seen']);
             Route::post('/remove-chat', [TeamChatController::class, 'removeChat']);
             Route::post('/clear-all-conversation', [TeamChatController::class, 'clearAllConversation']);
             Route::post('/notification', [TeamChatController::class, 'notification']);
@@ -143,30 +99,30 @@ Route::prefix('v3')->group(function () {
 
 
         Route::prefix('mail')->group(function () {
-    
+
             Route::post('/inbox-email-list', [EmailController::class, 'inboxEmailList']);
             Route::post('/sent-email-list', [EmailController::class, 'sentEmailList']);
             Route::post('/star-email-list', [EmailController::class, 'starEmailList']);
             Route::post('/trash-email-list', [EmailController::class, 'trashEmailList']);
-    
+
             Route::post('/view', [EmailController::class, 'view']);
 
             Route::post('/compose-mail-users', [EmailController::class, 'composeMailUsers']);
             Route::post('/send', [EmailController::class, 'sendMail']);
             Route::post('/sendReply', [EmailController::class, 'sendMailReply']);
             Route::post('/sendForward', [EmailController::class, 'sendMailForward']);
-            
+
             Route::post('/mail-delete', [EmailController::class, 'delete']);
             Route::post('/mail-delete-sent', [EmailController::class, 'deleteSentsMail']);
             Route::post('/mail-restore', [EmailController::class, 'restore']);
             Route::post('/mail-star', [EmailController::class, 'star']);
-    
+
             Route::post('/mail-delete-single', [EmailController::class, 'deleteSingleMail']);
             Route::post('/mail-restore-single', [EmailController::class, 'restoreSingleMail']);
-            
+
             Route::get('/print/{id}', [EmailController::class, 'printMail']);
-    
-            
+
+
         });
 
 
@@ -215,7 +171,7 @@ Route::prefix('v3')->group(function () {
             Route::get('/download/{file}', [LeadershipDevelopmentController::class, 'download']);
         });
 
-        
+
 
 
 
