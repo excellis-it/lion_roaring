a000a769896b3fb1fad4a7534d3810dac7edac96
masum-excellisit <masum@excellisit.net>
2024-11-12 18:59:56 +0530

    footer flag image added in e-store footer

--- resources/views/ecom/includes/footer.blade.php
+++ resources/views/ecom/includes/footer.blade.php
@@ -6,10 +6,33 @@
         <div class="row">
             <div class="col-lg-4 col-md-6">
                 <div class="left_logo me-0 me-xl-5">
-                    <div class="ftr_logo mb-3">
+                    {{-- <div class="ftr_logo mb-3">
                         <img src="{{ Helper::getFooterCms() ? Storage::url(Helper::getFooterCms()->footer_logo) : asset('ecom_assets/images/logo.png') }}"
                             alt="logo" />
+                    </div> --}}
+                    <div class="ftr_logo_sec">
+                        <div class="d-flex align-items-center">
+                            <div>
+                                <a class="ftr_logo">
+                                    <img src="{{ Helper::getFooterCms() ? Storage::url(Helper::getFooterCms()->footer_logo) : asset('ecom_assets/images/logo.png') }}"
+                                        alt="logo" />
+                                </a>
+                            </div>
+                            <div>
+                                <a class="ftr_logo_right">
+                                    @if (isset(Helper::getFooter()['footer_flag']))
+                                        <img src="{{ Storage::url(Helper::getFooter()['footer_flag']) }}"
+                                            alt="">
+                                    @else
+                                        <img src="{{ asset('frontend_assets/uploads/2024/02/Group-2029.png') }}"
+                                            alt="">
+                                    @endif
+                                </a>
+                            </div>
+                        </div>
                     </div>
+
+
                     <p>
                         {!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_title : '' !!}
                     </p>
@@ -32,9 +55,9 @@
                         <div class="ftr_text_h">
                             <p><b>{!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_address_title : '' !!}
 
-                            </b><br>
-                            {!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_address : '' !!}
-                        </p>
+                                </b><br>
+                                {!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_address : '' !!}
+                            </p>
                         </div>
                     </div>
                     <div class="d-flex align-items-center mb-3">
@@ -61,24 +84,23 @@
                         {!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_newsletter_title : '' !!}
                     </h4>
                     <div class="ftr-frm">
-                        <form action="{{route('e-store.newsletter')}}" method="post"  id="submit-newsletter">
+                        <form action="{{ route('e-store.newsletter') }}" method="post" id="submit-newsletter">
                             @csrf
                             <div class="row">
                                 <div class="form-group col-lg-6 col-md-12">
                                     <input size="40" class="form-control" id="newsletter_name"
-                                        placeholder="Full Name" value="" type="text"
-                                        name="newsletter_name">
-                                        <span class="text-danger" id="newsletter_name_error"></span>
+                                        placeholder="Full Name" value="" type="text" name="newsletter_name">
+                                    <span class="text-danger" id="newsletter_name_error"></span>
                                 </div>
                                 <div class="form-group col-lg-6 col-md-12">
                                     <input class="form-control" placeholder="Email ADDRESS" id="newsletter_email"
                                         value="" type="email" name="newsletter_email">
-                                        <span class="text-danger" id="newsletter_email_error"></span>
+                                    <span class="text-danger" id="newsletter_email_error"></span>
                                 </div>
                                 <div class="form-group col-12">
-                                    <textarea cols="40" rows="3" class="form-control" id="newsletter_message"
-                                        placeholder="Message" name="newsletter_message"></textarea>
-                                        <span class="text-danger" id="newsletter_message_error"></span>
+                                    <textarea cols="40" rows="3" class="form-control" id="newsletter_message" placeholder="Message"
+                                        name="newsletter_message"></textarea>
+                                    <span class="text-danger" id="newsletter_message_error"></span>
                                 </div>
                             </div>
                             <div class="main-btn">
@@ -117,12 +139,12 @@
                     <li><a href="{{ route('contact-us') }}">Contact us</a></li>
                     @if (Helper::getCmsPages() && count(Helper::getCmsPages()) > 0)
                         @foreach (Helper::getCmsPages() as $page)
-                            <li><a href="{{ route($page->slug. '.cms-page') }}">{{ $page->page_name }}</a></li>
+                            <li><a href="{{ route($page->slug . '.cms-page') }}">{{ $page->page_name }}</a></li>
                         @endforeach
                     @endif
                 </ul>
             </div>
-            <p>     {!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_copywrite_text : '' !!}</p>
+            <p> {!! Helper::getFooterCms() ? Helper::getFooterCms()->footer_copywrite_text : '' !!}</p>
         </div>
 
     </div>
