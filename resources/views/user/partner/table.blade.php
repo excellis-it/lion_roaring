f6c835ee207a73dcdac7b918613806ecb9a50ae1
masum-excellisit <masum@excellisit.net>
2025-02-18 11:09:49 +0530

    member list

--- resources/views/user/partner/table.blade.php
+++ resources/views/user/partner/table.blade.php
@@ -16,6 +16,48 @@
                         data-bs-toggle="modal" data-bs-target="#modalEcAccess_{{ $partner->id }}">
                         <i class="ti ti-eye"></i> Ecclesia Access
                     </button>
+
+                    <!-- Modal Body -->
+                    <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                    <div class="modal fade" id="modalEcAccess_{{ $partner->id }}" tabindex="-1" data-bs-backdrop="static"
+                        data-bs-keyboard="false" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title">
+                                        House Of Ecclesia Access for : <br>{{ $partner->full_name }}
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+
+                                    <ul class="list-group list-group-numbered">
+                                        @if (!empty($partner->ecclesia_access))
+                                            @foreach ($partner->ecclesia_access as $ecclesia_access)
+                                                <li class="list-group-item mb-1">{{ $ecclesia_access->name }}
+                                                </li>
+                                            @endforeach
+                                        @endif
+
+                                    </ul>
+
+                                </div>
+                                <div class="modal-footer">
+                                    @if (auth()->user()->can('Edit Partners'))
+                                        <a href="{{ route('partners.edit', Crypt::encrypt($partner->id)) }}"
+                                            type="button" class="btn btn-primary me-3">
+                                            Edit Member
+                                        </a>
+                                    @endif
+                                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                                        Close
+                                    </button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endif
             </td>
 
@@ -24,8 +66,8 @@
             </td>
             {{-- <td>{{ $partner->user_name }}</td>
 
-            <td>{{ $partner->phone }}</td>
-            <td>{{ $partner->address }}</td> --}}
+    <td>{{ $partner->phone }}</td>
+    <td>{{ $partner->address }}</td> --}}
 
             @if (auth()->user()->can('Edit Partners'))
                 <td>
@@ -43,13 +85,15 @@
                 <td>
                     <div class="d-flex">
                         @if (Auth::user()->can('Edit Partners'))
-                            <a href="{{ route('partners.edit', Crypt::encrypt($partner->id)) }}" class="edit_icon me-2">
+                            <a href="{{ route('partners.edit', Crypt::encrypt($partner->id)) }}"
+                                class="edit_icon me-2">
                                 <i class="ti ti-edit"></i>
                             </a>
                         @endif
 
                         @if (Auth::user()->can('View Partners'))
-                            <a href="{{ route('partners.show', Crypt::encrypt($partner->id)) }}" class="view_icon me-2">
+                            <a href="{{ route('partners.show', Crypt::encrypt($partner->id)) }}"
+                                class="view_icon me-2">
                                 <i class="ti ti-eye"></i>
                             </a>
                         @endif
@@ -85,45 +129,5 @@
 
 @if (count($partners) > 0)
     @foreach ($partners as $key => $partner)
-        <!-- Modal Body -->
-        <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-        <div class="modal fade" id="modalEcAccess_{{ $partner->id }}" tabindex="-1" data-bs-backdrop="static"
-            data-bs-keyboard="false" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">
-                            House Of Ecclesia Access for : <br>{{ $partner->full_name }}
-                        </h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-
-                        <ul class="list-group list-group-numbered">
-                            @if (!empty($partner->ecclesia_access))
-                                @foreach ($partner->ecclesia_access as $ecclesia_access)
-                                    <li class="list-group-item mb-1">{{ $ecclesia_access->name }}
-                                    </li>
-                                @endforeach
-                            @endif
-
-                        </ul>
-
-                    </div>
-                    <div class="modal-footer">
-                        @if (auth()->user()->can('Edit Partners'))
-                            <a href="{{ route('partners.edit', Crypt::encrypt($partner->id)) }}" type="button"
-                                class="btn btn-primary me-3">
-                                Edit Member
-                            </a>
-                        @endif
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
-                            Close
-                        </button>
-
-                    </div>
-                </div>
-            </div>
-        </div>
     @endforeach
 @endif
