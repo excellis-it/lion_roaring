5bf0fc68b8c0e17cb846124f80cd0b278824c143
masum-excellisit <masum@excellisit.net>
2025-06-16 19:56:46 +0530

    fix: optimize data retrieval in HomeTransformers and improve formatting in footer view

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -9,46 +9,54 @@
                     <div class="ftr_logo_sec">
                         <div class="d-flex align-items-center">
                             <div>
-                                <a  class="ftr_logo">
+                                <a class="ftr_logo">
                                     @if (isset(Helper::getFooter()['footer_logo']))
-                                        <img src="{{Storage::url(Helper::getFooter()['footer_logo'])}}" alt="">
+                                        <img src="{{ Storage::url(Helper::getFooter()['footer_logo']) }}" alt="">
                                     @else
-                                    <img src="{{asset('frontend_assets/uploads/2024/02/Group-2029.png')}}" alt="">
+                                        <img src="{{ asset('frontend_assets/uploads/2024/02/Group-2029.png') }}"
+                                            alt="">
                                     @endif
                                 </a>
                             </div>
                             <div>
-                                <a  class="ftr_logo_right">
+                                <a class="ftr_logo_right">
                                     @if (isset(Helper::getFooter()['footer_flag']))
-                                        <img src="{{Storage::url(Helper::getFooter()['footer_flag'])}}" alt="">
+                                        <img src="{{ Storage::url(Helper::getFooter()['footer_flag']) }}"
+                                            alt="">
                                     @else
-                                    <img src="{{asset('frontend_assets/uploads/2024/02/Group-2029.png')}}" alt="">
+                                        <img src="{{ asset('frontend_assets/uploads/2024/02/Group-2029.png') }}"
+                                            alt="">
                                     @endif
                                 </a>
                             </div>
                         </div>
 
                         <p>
-                            {!! Helper::getFooter()['footer_title'] ?? 'Our main focus is to restore our various communities, villages, cities, states,
-                            and
-                            our nation by restoring the condition of a person in both the spiritual and the
-                            physical.'!!}
-                            </p>
+                            {!! Helper::getFooter()['footer_title'] ??
+                                'Our main focus is to restore our various communities, villages, cities, states,
+                                                        and
+                                                        our nation by restoring the condition of a person in both the spiritual and the
+                                                        physical.' !!}
+                        </p>
                         <div class="col-lg-12">
                             <div class="d-flex align-items-center">
-                                <a href="{{Helper::getFooter()['footer_playstore_link'] ?? 'javascript:void(0);'}}" class="me-2">
+                                <a href="{{ Helper::getFooter()['footer_playstore_link'] ?? 'javascript:void(0);' }}"
+                                    class="me-2">
                                     @if (isset(Helper::getFooter()['footer_playstore_link']))
-                                        <img src="{{Storage::url(Helper::getFooter()['footer_playstore_image'])}}" alt="">
+                                        <img src="{{ Storage::url(Helper::getFooter()['footer_playstore_image']) }}"
+                                            alt="">
                                     @else
-                                    <img src="{{asset('frontend_assets/uploads/2024/01/playstore.png')}}"
-                                        alt="">
+                                        <img src="{{ asset('frontend_assets/uploads/2024/01/playstore.png') }}"
+                                            alt="">
                                     @endif
-                                    </a>
-                                <a href="{{Helper::getFooter()['footer_appstore_link'] ?? 'javascript:void(0);'}}">
+                                </a>
+                                <a href="{{ Helper::getFooter()['footer_appstore_link'] ?? 'javascript:void(0);' }}">
                                     @if (isset(Helper::getFooter()['footer_appstore_link']))
-                                        <img src="{{Storage::url(Helper::getFooter()['footer_appstore_image'])}}" alt="">
+                                        <img src="{{ Storage::url(Helper::getFooter()['footer_appstore_image']) }}"
+                                            alt="">
                                     @else
-                                    <img src="{{asset('frontend_assets/uploads/2024/01/appstore.png')}}" alt="">
+                                        <img src="{{ asset('frontend_assets/uploads/2024/01/appstore.png') }}"
+                                            alt="">
                                     @endif
                                 </a>
                             </div>
@@ -64,24 +72,25 @@
                                     <p role="status" aria-live="polite" aria-atomic="true"></p>
                                     <ul></ul>
                                 </div>
-                                <form action="{{route('newsletter')}}" method="post"  id="submit-newsletter">
+                                <form action="{{ route('newsletter') }}" method="post" id="submit-newsletter">
                                     @csrf
                                     <div class="row">
                                         <div class="form-group col-lg-6 col-md-12">
                                             <input size="40" class="form-control" id="newsletter_name"
                                                 placeholder="Full Name" value="" type="text"
                                                 name="newsletter_name">
-                                                <span class="text-danger" id="newsletter_name_error"></span>
+                                            <span class="text-danger" id="newsletter_name_error"></span>
                                         </div>
                                         <div class="form-group col-lg-6 col-md-12">
-                                            <input class="form-control" placeholder="Email ADDRESS" id="newsletter_email"
-                                                value="" type="email" name="newsletter_email">
-                                                <span class="text-danger" id="newsletter_email_error"></span>
+                                            <input class="form-control" placeholder="Email ADDRESS"
+                                                id="newsletter_email" value="" type="email"
+                                                name="newsletter_email">
+                                            <span class="text-danger" id="newsletter_email_error"></span>
                                         </div>
                                         <div class="form-group col-12">
-                                            <textarea cols="40" rows="10" class="form-control" id="newsletter_message"
-                                                placeholder="Message" name="newsletter_message"></textarea>
-                                                <span class="text-danger" id="newsletter_message_error"></span>
+                                            <textarea cols="40" rows="10" class="form-control" id="newsletter_message" placeholder="Message"
+                                                name="newsletter_message"></textarea>
+                                            <span class="text-danger" id="newsletter_message_error"></span>
                                         </div>
                                     </div>
                                     <div class="main-btn">
@@ -104,17 +113,19 @@
                     <div class="quick_links_ul">
                         <ul class="menu" style="white-space: nowrap">
                             <li class="active">
-                                <a href="{{route('home')}}" aria-current="page">Home</a>
+                                <a href="{{ route('home') }}" aria-current="page">Home</a>
                             </li>
                             <li>
-                                <a href="{{Helper::getPDFAttribute() ?? 'javascript:void(0);'}}"  target="_blank">Article of
-                                    Association</a>
+                                <a href="{{ Helper::getPDFAttribute() ?? 'javascript:void(0);' }}"
+                                    target="_blank">Article of
+                                    Agreement</a>
                             </li>
                             <li class="active">
-                                <a href="{{route('privacy-policy')}}" aria-current="page">Privacy Policy</a>
+                                <a href="{{ route('privacy-policy') }}" aria-current="page">Privacy Policy</a>
                             </li>
                             <li class="active">
-                                <a href="{{route('terms-and-conditions')}}" aria-current="page">Terms and Conditions</a>
+                                <a href="{{ route('terms-and-conditions') }}" aria-current="page">Terms and
+                                    Conditions</a>
                             </li>
                             {{-- <li>
                                 <a href="{{route('contact-us')}}">Contact Us</a>
@@ -123,7 +134,8 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <p>{!! Helper::getFooter()['footer_copywrite_text'] ?? 'Copyright © '. date('Y') .' Daud Santosa. All Rights Reserved' !!}</p>
+                    <p>{!! Helper::getFooter()['footer_copywrite_text'] ??
+                        'Copyright © ' . date('Y') . ' Daud Santosa. All Rights Reserved' !!}</p>
                 </div>
             </div>
         </div>
