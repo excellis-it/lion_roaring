42b7187ddfe59a05cddc5190855e61ed7127094c
excellis-it <web.excellisit@gmail.com>
2025-10-15 19:07:29 +0530

    small space

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call([
             // assignRoleSeeder::class,
-            // assignAdminSeeder::class,
+            // assignAdminSeeder::class, 
             // CountriesTableSeeder::class,
             // CategorySeeder::class,
             // RolePermissionSeeder::class,
