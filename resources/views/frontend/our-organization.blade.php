4320a63a2df028c932258ad5aee30b38d95e4048
masum-excellisit <masum@excellisit.net>
2025-12-29 20:22:06 +0530

    fix: Improve code formatting and consistency in our-organization view

--- resources/views/frontend/our-organization.blade.php
+++ resources/views/frontend/our-organization.blade.php
@@ -2,19 +2,19 @@
 @section('meta_title')
 @endsection
 @section('title')
-    {{ env('APP_NAME') }} - {{$our_organization['meta_title'] ?? 'Ecclesia Association'}}
+    {{ env('APP_NAME') }} - {{ $our_organization['meta_title'] ?? 'Ecclesia Association' }}
 @endsection
 @push('styles')
 @endpush
 
 @section('content')
     <section class="inner_banner_sec"
-        style="background-image: url({{asset('frontend_assets/uploads/2023/07/inner_banner.jpg')}}); background-position: center; background-repeat: no-repeat; background-size: cover">
+        style="background-image: url({{ asset('frontend_assets/uploads/2023/07/inner_banner.jpg') }}); background-position: center; background-repeat: no-repeat; background-size: cover">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="inner_banner_ontent text-center">
-                        <h1>{{$our_organization['name'] ?? 'title'}}</h1>
+                        <h1>{{ $our_organization['name'] ?? 'title' }}</h1>
                     </div>
                 </div>
             </div>
@@ -29,33 +29,34 @@
             <div class="row align-items-center justify-content-center mb-5">
                 <div class="col-lg-8">
                     <div class="text-center">
-                        <h2>{{$our_organization['description'] ?? 'description'}}</h2>
+                        <h2>{!! $our_organization['description'] ?? 'description' !!}</h2>
                     </div>
                 </div>
             </div>
             <div class="slid_bh">
                 @if (count($organization_centers) > 0)
                     @foreach ($organization_centers as $key => $organization_center)
-                    <div class="padding_k">
-                        <div class="{{($key % 2 == 0 ) ? 'bounce_1' : 'bounce_2' }}">
-                            <div class="{{($key % 2 == 0 ) ? 'one_cli' : 'one_cli1' }}">
-                                <div class="one_cli_nh">
-                                    <img src="{{ asset('frontend_assets/images/before_n.png') }}" alt="">
-                                </div>
-                                <div class="clild_box">
-                                    <div class="clild_sec">
-                                        <img src="{{Storage::url($organization_center->image) ?? 'https://via.placeholder.com/150'}}"
-                                            alt="">
-                                        <h4>{{$organization_center->name}}</h4>
-
+                        <div class="padding_k">
+                            <div class="{{ $key % 2 == 0 ? 'bounce_1' : 'bounce_2' }}">
+                                <div class="{{ $key % 2 == 0 ? 'one_cli' : 'one_cli1' }}">
+                                    <div class="one_cli_nh">
+                                        <img src="{{ asset('frontend_assets/images/before_n.png') }}" alt="">
                                     </div>
-                                    <a href="{{route('features', $organization_center->slug)}}" class="ellipss_right" tabindex="-1">
-                                        <i class="fa-solid fa-ellipsis"></i>
-                                    </a>
+                                    <div class="clild_box">
+                                        <div class="clild_sec">
+                                            <img src="{{ Storage::url($organization_center->image) ?? 'https://via.placeholder.com/150' }}"
+                                                alt="">
+                                            <h4>{{ $organization_center->name }}</h4>
+
+                                        </div>
+                                        <a href="{{ route('features', $organization_center->slug) }}" class="ellipss_right"
+                                            tabindex="-1">
+                                            <i class="fa-solid fa-ellipsis"></i>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     @endforeach
                 @endif
 
