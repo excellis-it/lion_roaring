8c61c633a45d187e063a04b746411fa13489ea84
excellis-it <web.excellisit@gmail.com>
2026-02-06 12:55:14 +0530

    add

--- app/Http/Controllers/User/TeamChatController.php
+++ app/Http/Controllers/User/TeamChatController.php
@@ -67,9 +67,28 @@ class TeamChatController extends Controller
 
 
 
-            $members = User::with('roles')->orderBy('first_name', 'asc')->where('id', '!=', auth()->id())->where('status', true)->whereHas('roles', function ($query) {
-                $query->whereIn('type', [1, 2, 3]);
-            })->get();
+            $user_type = auth()->user()->user_type;
+            $country_name = auth()->user()->country;
+            $isSuperAdmin = auth()->user()->hasNewRole('SUPER ADMIN');
+
+            $membersQuery = User::with('roles')
+                ->orderBy('first_name', 'asc')
+                ->where('id', '!=', auth()->id())
+                ->where('status', true)
+                ->whereHas('roles', function ($query) {
+                    $query->whereIn('type', [1, 2, 3]);
+                });
+
+            if (!$isSuperAdmin) {
+                if ($user_type == 'Global') {
+                    $membersQuery->where('user_type', 'Global');
+                } else {
+                    $membersQuery->where('user_type', 'Regional')->where('country', $country_name);
+                }
+            }
+
+            $members = $membersQuery->get();
+
             return view('user.team-chat.index')->with(compact('teams', 'members'));
         } else {
             abort(403, 'You do not have permission to access this page.');
@@ -371,9 +390,27 @@ class TeamChatController extends Controller
                     $query->where('is_removed', false); // Replace with your condition
                 }, 'members.user'])
                 ->first();
-            $members = User::with('roles')->orderBy('first_name', 'asc')->where('id', '!=', auth()->id())->where('status', true)->whereHas('roles', function ($query) {
-                $query->whereIn('type', [1, 2, 3]);
-            })->get();
+            $user_type = auth()->user()->user_type;
+            $country_name = auth()->user()->country;
+            $isSuperAdmin = auth()->user()->hasNewRole('SUPER ADMIN');
+
+            $membersQuery = User::with('roles')
+                ->orderBy('first_name', 'asc')
+                ->where('id', '!=', auth()->id())
+                ->where('status', true)
+                ->whereHas('roles', function ($query) {
+                    $query->whereIn('type', [1, 2, 3]);
+                });
+
+            if (!$isSuperAdmin) {
+                if ($user_type == 'Global') {
+                    $membersQuery->where('user_type', 'Global');
+                } else {
+                    $membersQuery->where('user_type', 'Regional')->where('country', $country_name);
+                }
+            }
+
+            $members = $membersQuery->get();
             $is_group_info = true;
             return response()->json(['view' => (string) view('user.team-chat.group-info')->with(compact('team', 'is_group_info', 'members'))]);
         }
